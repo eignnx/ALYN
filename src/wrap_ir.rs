127ec957c293aadf9528e676b4f7bc43c5ddcb1c
Gideon <eignnx@gmail.com>
2025-08-16 12:20:11 -0400

    document, fix typo

--- src/wrap_ir.rs
+++ src/wrap_ir.rs
@@ -6,6 +6,22 @@ use crate::ir::{Binop, LVal, Lbl, RVal, Relop, Stmt, Tmp};
 pub enum IrWrap {
     RVal(RVal),
     Stmt(Stmt),
+    /// A closure that produces a statement from a conditional once fed both an
+    /// `if_true` label and an `if_false` label.
+    ///
+    /// ```text
+    /// // `x > y`
+    /// Cond(|t, f| Br { op: Gt, e1: x, e2: y, if_true: t, if_false: f })
+    ///
+    /// // `if x > y { 't: (); } else { 'f: (); }`
+    /// [
+    ///     Br { op: Gt, e1: x, e2: y, if_true: t, if_false: f },
+    ///     Lbl("t"),
+    ///     Jmp("end", ["end"]),
+    ///     Lbl("f"),
+    ///     Lbl("end"),
+    /// ]
+    /// ```
     Cond(Box<dyn Fn(Lbl, Lbl) -> Stmt>),
 }
 
@@ -47,8 +63,8 @@ impl IrWrap {
         match self {
             IrWrap::Cond(mk_stmt) => mk_stmt,
             IrWrap::Stmt(_) => unreachable!("Statement inside condition should never occur"),
-            IrWrap::RVal(RVal::Int(0)) => Box::new(|_t, f| Stmt::direct_jmp(f))
-            IrWrap::RVal(RVal::Int(0)) => Box::new(|t, _f| Stmt::direct_jmp(t))
+            IrWrap::RVal(RVal::Int(0)) => Box::new(|_t, f| Stmt::direct_jmp(f)),
+            IrWrap::RVal(RVal::Int(0)) => Box::new(|t, _f| Stmt::direct_jmp(t)),
             IrWrap::RVal(rval) => Box::new(move |t, f| Stmt::Br {
                 op: Relop::Ne,
                 e1: rval.clone(),
